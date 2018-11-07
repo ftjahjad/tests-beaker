f68d984d3a3f95d3a7c1fc353df995fa3867e619
Fendy Tjahjadi <ftjahjad@redhat.com>
2018-11-07 15:42:36 -0500

    Initial check-in for sanitized acpi

---
+++ acpi/enabled/runtest.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Include Beaker environment
+. /usr/bin/rhts-environment.sh || exit 1
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+# verify to run on aarch64
+if [ "$(uname -i)" != "aarch64" ]; then
+    rhts-report-result $TEST SKIP $OUTPUTFILE
+else
+    rlJournalStart
+
+        rlPhaseStartTest
+            [ -f /var/log/dmesg ] && \
+                rlAssertNotGrep "ACPI: Interpreter disabled" /var/log/dmesg
+
+            dmesg > /tmp/dmesg.out
+            rlAssertNotGrep "ACPI: Interpreter disabled" /tmp/dmesg.out
+
+            journalctl -b0 > /tmp/journal.out
+            rlAssertNotGrep "ACPI: Interpreter disabled" /tmp/journal.out
+
+        rlPhaseEnd
+
+    rlJournalPrintText
+    rlJournalEnd
+fi
